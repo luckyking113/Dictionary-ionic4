4635463660602627587bb1dd15813754e1b72fde
luckyking113 <TopDev19930328@outlook.com>
2019-02-27 18:24:19 +0200

    add animation for search word tab

--- src/app/pages/word-search/word-search.page.ts
+++ src/app/pages/word-search/word-search.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, Renderer2 } from '@angular/core';
 
 import dicdata from '../../dicdata/dicdata';
 import * as _ from 'lodash';
@@ -13,20 +13,22 @@ import { AdMobFree, AdMobFreeBannerConfig} from '@ionic-native/admob-free/ngx';
 })
 export class WordSearchPage implements OnInit {
 
-  @ViewChild('slider') slider: IonSlides;   
+  @ViewChild('slider') slider: IonSlides;    
   page = 0;
   searchResults:{
-    word_id: string,
+    word_id: number,
     Eword: string,
     Engmeaning: string,
     Chimeaning: string,
+    ChForEng: string,
     Kameaning: string,
+    KaForEng: string
   }[];
 
   allWords = [];  
   keyword:string;   
 
-  constructor(public navCtrl: NavController, public admobFree: AdMobFree,  private platform: Platform) { 
+  constructor(public navCtrl: NavController, public admobFree: AdMobFree,  private platform: Platform,private rd: Renderer2) { 
     this.allWords = dicdata[0].searchkeyword;    
     this.getAdmobFree();
   }
@@ -53,8 +55,7 @@ export class WordSearchPage implements OnInit {
   ngOnInit() { }
 
   searchkeyword(ev){    
-    //65 ~ 90  97~122  
-    
+    //65 ~ 90  97~122      
     if (((ev.target.value.charCodeAt(0) >= 97) && (ev.target.value.charCodeAt(0) <= 122))
         ||((ev.target.value.charCodeAt(0) >= 65) && (ev.target.value.charCodeAt(0) <= 90))){
       var asciicode = ev.target.value.charCodeAt(0);      
@@ -69,9 +70,13 @@ export class WordSearchPage implements OnInit {
         this.allWords = dicdata[0].searchkeyword;
       }
       else {     
-        var searchword = ev.target.value.toLowerCase();        
+        var searchword = ev.target.value.toLowerCase();      
+          
         let words = _.filter(dicdata[searchIndex].searchkeyword, t=>(<any>t).word.toLowerCase().includes(searchword));
+        var katchtextforeng = words[0].Kameaning;
         this.allWords = words;
+        console.log(katchtextforeng);
+        console.log(this.allWords);
       }
     } else {      
       console.log("insert correct word");
@@ -83,11 +88,29 @@ export class WordSearchPage implements OnInit {
   }
 
   toggleSection(i) {
+    console.log(i);
     this.allWords[i].open = !this.allWords[i].open;
+    var txtForClass = "." + "changeColor" + i.toString();
+    var txtForClass1 = "." + "changeColor1" + i.toString();  
+    var txtForClass2 = "." + "changeColor2" + i.toString();      
+
+    let shadesEl=document.querySelector(txtForClass); 
+    let shadesEl1=document.querySelector(txtForClass1); 
+    let shadesEl2=document.querySelector(txtForClass2);    
+
+    console.log(shadesEl.classList);
+    if (shadesEl.classList.contains('changeColor')) {
+      shadesEl.classList.remove('changeColor');
+      shadesEl1.classList.remove('changeColor');
+      shadesEl2.classList.remove('changeColor');
+    } else {
+      shadesEl.classList.add('changeColor');
+      shadesEl1.classList.add('changeColor');
+      shadesEl2.classList.add('changeColor');
+    }
   }
 
   toggleItem(i){
     this.allWords[i].open = !this.allWords[i].open;
   }
-
 }
